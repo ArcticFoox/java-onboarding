1c74391a4e737bfc6d8842c304f654faae7a8f6c
신종신 <jongsin16@naver.com>
2024-09-24 23:00:54 +0900

    problem2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,38 @@
 package onboarding;
 
+import java.util.Stack;
+
 public class Problem2 {
+
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        Problem2 T = new Problem2();
+        String decryptogram = "";
+        decryptogram = T.decrypt(cryptogram);
+
+        return decryptogram;
+    }
+
+    public String decrypt(String cryptogram) {
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : cryptogram.toCharArray()) {
+            compareToGrams(stack, c);
+        }
+
+        while (!stack.empty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.reverse().toString();
+    }
+
+    public void compareToGrams(Stack<Character> stack, char c) {
+        if (!stack.empty() && stack.peek() == c) {
+            stack.pop();
+            return;
+        }
+
+        stack.add(c);
     }
 }
